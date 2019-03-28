2e1ba7a53a8a3f208a0adaed5c5d6cab973ef14c
RG.xxvi-xxxi <rdimasapit.bos@gmail.com>
2019-03-28 14:40:02 +0800

    Added response headers

--- app.js
+++ app.js
@@ -24,6 +24,11 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
   extended: false
 }))
+app.use(function(req,res,next){
+  res.header('Access-Control-Allow-Origin','*')
+  res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
 
 const loginCtrl = require('./controllers/login')
 const regCtrl = require('./controllers/register')
